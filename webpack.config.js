f745e4c3bef5aae2d69cbbc90978bcdf777e01b3
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-10-06 19:14:15 -1000

    Configured webpack with react

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    home: "./src/pages/Index.jsx",
+    // Add more when the time comes.
+  },
+  output: {
+    path: path.resolve(__dirname, "./public/dist"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
