42772739aaa04822bf58d4db7f4c3a330ae57985
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-10-09 23:08:33 -1000

    Initial Push: Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import "../styles/styles.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <div>Hello World</div>
+    </>
+  );
+};
+
+export default Navbar;
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import "../styles/styles.css";
+import Navbar from "../components/Navbar";
 
 const Index = () => {
   return (
     <>
+      <Navbar />
       <div>Hello World!</div>
     </>
   );
