7bcb76a1846d9ed299324d6a189921f453730cb2
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-10-28 21:54:47 -1000

    Modified files

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import "../styles/styles.css";
-import Header from "../components/Header";
 
 const Index = () => {
   return (
