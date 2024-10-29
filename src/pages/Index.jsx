32130f09f8c7c6d6608144854422b017263ada7c
Levi Kuhaulua <126765430+LeviKuhaulua@users.noreply.github.com>
2024-10-29 10:14:48 -1000

    Removed redundant Header import.

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import "../styles/styles.css";
-import "../components/Header";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
