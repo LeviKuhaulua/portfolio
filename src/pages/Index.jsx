fcf45bd468c0a83f10d7f3cf8d5ce4565e3db797
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-10-30 22:39:38 -1000

    Added Navbar component to index page

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "../styles/styles.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 const Index = () => {
   return (
     <div className="min-h-screen text-white bg-background">
       <Header />
+      <Navbar />
       <div>Hello World</div>
       <Footer />
     </div>
