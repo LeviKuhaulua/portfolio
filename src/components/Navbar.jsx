b5b15baf88b55f5e356f08e6eb5250e245c56c8b
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-10-31 22:39:07 -1000

    Refactored navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,7 @@ import "../styles/styles.css";
 const Navbar = (props) => {
   return (
     <nav className="flex flex-col justify-start items-start gap-8 px-4 py-3.5 md:gap-4 md:px-8 md:flex-row">
-      {props.children.map((btn, index) => (
-        <li key={index} className="list-none">
-          {btn}
-        </li>
-      ))}
+      {props.children}
     </nav>
   );
 };
