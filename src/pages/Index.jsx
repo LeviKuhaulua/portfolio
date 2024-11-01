c78e7e6c79a659fc178092effa747888f2478b4d
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-10-31 21:57:42 -1000

    Added navbar and button component to Index page

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -4,12 +4,17 @@ import "../styles/styles.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import Button from "../components/Button";
 
 const Index = () => {
   return (
     <div className="min-h-screen text-white bg-background">
       <Header />
-      <Navbar />
+      <Navbar>
+        <Button name="projects" />
+        <Button name="experience" />
+        <Button name="download resume" download="path/to/something" />
+      </Navbar>
       <div>Hello World</div>
       <Footer />
     </div>
