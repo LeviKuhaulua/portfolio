1c4153d1ddf5b0300a63a079dc641a040c0a8488
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-10-31 14:10:03 -1000

    General template for navbar created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import * as React from "react";
 import "../styles/styles.css";
 import { useState } from "react";
+import Button from "./Button";
 
 const Navbar = (props, children) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <>
-      <p>Hello World</p>
-    </>
+    <nav className="flex flex-row gap-2.5 px-4 md:px-8">
+      <Button name="skills" />
+      <Button name="experience" />
+      <Button name="download resume" download="../../public/assets/LK.svg" />
+    </nav>
   );
 };
 
