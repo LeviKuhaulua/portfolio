753920a5837123d099dd9b1748693c98dd9c7b38
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-10-31 21:55:37 -1000

    Implemented design of navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import "../styles/styles.css";
 import { useState } from "react";
 import Button from "./Button";
 
-const Navbar = (props, children) => {
-  const [open, setOpen] = useState(false);
-
+const Navbar = (props) => {
   return (
-    <nav className="flex flex-row gap-2.5 px-4 md:px-8">
-      <Button name="skills" />
-      <Button name="experience" />
-      <Button name="download resume" download="../../public/assets/LK.svg" />
+    <nav className="flex flex-col justify-start items-start gap-8 px-4 py-3.5 md:gap-4 md:px-8 md:flex-row">
+      {props.children.map((btn, index) => (
+        <li key={index} className="list-none">
+          {btn}
+        </li>
+      ))}
     </nav>
   );
 };
