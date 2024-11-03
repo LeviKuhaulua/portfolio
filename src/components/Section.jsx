7a24ab672a52ee55acb202ea8045c3dea7bbd9e3
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-11-02 23:37:57 -1000

    Finished Section component for now...

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -4,7 +4,18 @@ import "../styles/styles.css";
 const Section = (props) => {
   return (
     <>
-      <h1>{props.name}</h1>
+      <section id={props.name} className="flex flex-col gap-4.5 py-5">
+        <div
+          className="h-[5px] rounded-md border border-[#CED1A1] content-none"
+          role="presentation"
+        ></div>
+        <p className="text-2xl font-medium text-text capitalize">
+          {props.name}
+        </p>
+
+        {/* TODO: After creating experience and project section, add the children here and style it */}
+        {/* {props.children} */}
+      </section>
     </>
   );
 };
