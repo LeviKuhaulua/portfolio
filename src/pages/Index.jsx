f2e27ede3cde8ef97404e977b159ae96886af46e
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-11-07 17:09:27 -1000

    Added experience components with information on them

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -22,7 +22,18 @@ const Index = () => {
       <Section name="experience"></Section>
       <Experience
         title="Web Developer (Club Member)"
-        location="AUG 2024 - PRESENT | UH Mānoa"
+        location="Aug 2024 - Present | 8bit Club @ UH Mānoa"
+        duties="Implement responsive and user-friendly components and pages in React, Collaborate with other team members to complete tasks in a timely manner."
+      />
+      <Experience
+        title="Multimedia Center Technician"
+        location="Aug 2024 - Present | Kalakaua Middle School"
+        duties="Lead a team of 5 student workers to run and manage the eSports area and equipment, Streamlined and standardized workflows to simplify software updates and prevent unauthorized software installations"
+      />
+      <Experience
+        title="Technical Analyst Assistant"
+        location="Oct 2022 - May 2024 | Hawai'i Pacific University"
+        duties="Boosted teacher and student satisfaction by resolving over 1200 tickets on hardware and software related issues, Created over 30 pages of documentation on resolving complex issues - leading to an increase in response times"
       />
       <Footer />
     </div>
