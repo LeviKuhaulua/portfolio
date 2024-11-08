e73f3d8d4979bae1853028fdf92732d46373a3b3
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-11-07 17:08:58 -1000

    Impl: Duties information and responsiveness

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,12 +5,27 @@ const Experience = (props) => {
   return (
     <>
       <article>
-        <hgroup>
-          <h4 className="text-primary text-xl font-medium leading-8">
+        <hgroup className="mb-6">
+          <h4 className="text-primary text-sm font-medium leading-8 md:text-xl">
             {props.title}
           </h4>
-          <p className="text-accent text-sm font-thin">{props.location}</p>
+          <p className="text-accent text-xs font-thin md:text-sm">
+            {props.location}
+          </p>
         </hgroup>
+
+        <ul className="list-disc list-inside">
+          {props.duties
+            ? props.duties.split(",").map((value, index) => (
+                <li
+                  key={index}
+                  className="font-extralight text-xs text-pretty md:text-lg"
+                >
+                  {value}
+                </li>
+              ))
+            : null}
+        </ul>
       </article>
     </>
   );
