a05f7a4adfdd417886f7e659353f20b7e659fccd
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-11-08 22:17:02 -1000

    Created Project Component, skeleton

---
+++ src/components/Project.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { useState } from "react-dom";
+
+const Project = (props) => {
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
+  );
+};
+
+export default Project;
