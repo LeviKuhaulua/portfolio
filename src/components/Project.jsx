13eeca320ca547ac2c06181eb6dc604138e1dd65
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-11-09 20:49:13 -1000

    Impl: Project card w/ background image and learn more

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,36 @@
 import * as React from "react";
-import { useState } from "react-dom";
+import { useState } from "react";
 
 const Project = (props) => {
+  const [open, setOpen] = useState(false);
   return (
     <>
-      <h1>Hello World!</h1>
+      <article className="w-[325px]">
+        <button
+          type="button"
+          className="grid isolate relative after:content-[''] after:absolute after:top-0 after:left-0 after:w-full after:h-full after:block after:bg-gradient-to-b after:from-transparent after:from-20% after:to-black after:to-90%"
+          onClick={() => setOpen(!open)}
+        >
+          {!open ? (
+            <>
+              <p className="sr-only">
+                Click me to learn more about the {props.project} project
+              </p>
+              <img
+                src={props.link}
+                role="presentation"
+                className="col-span-full row-span-full object-fill"
+              />
+              <p className="col-span-full row-span-full self-end text-center text-xl font-bold my-6 text-white z-10">
+                Learn More
+              </p>
+            </>
+          ) : (
+            // Shows project info here.
+            <p>Hello World</p>
+          )}
+        </button>
+      </article>
     </>
   );
 };
