d8a3109a6db4fcdbd0438a01503a3594a5026685
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-11-17 22:46:07 -1000

    Modified component

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -6,10 +6,10 @@ const Section = (props) => {
     <>
       <section
         id={props.name}
-        className="flex flex-col gap-7 py-5 px-4 md:px-7 md:w-fit"
+        className="flex flex-col gap-5 py-5 px-4 md:px-7"
       >
         <div
-          className="h-[5px] rounded-md border border-[#CED1A1] content-none"
+          className="h-[5px] rounded-lg border border-[#CED1A1] content-none"
           role="presentation"
         ></div>
         <p className="text-2xl font-medium text-text capitalize">
@@ -19,7 +19,7 @@ const Section = (props) => {
         {/* Child Components */}
 
         {props.sidescroll ? (
-          <section className="grid grid-flow-col gap-3 overflow-x-auto snap-x snap-mandatory *:snap-always *:snap-center md:gap-5 lg:max-xl:grid-flow-row md:max-xl:grid-cols-2">
+          <section className="grid grid-flow-col gap-3 overflow-x-auto snap-x snap-mandatory *:snap-always *:snap-center md:gap-4">
             {props.children}
           </section>
         ) : (
