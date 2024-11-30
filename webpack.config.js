9a1508453631869e668da15131a242a3a0a971bf
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-11-29 18:29:44 -1000

    Minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     index: "./src/pages/Index.jsx",
   },
   output: {
-    path: path.resolve(__dirname, "./public/dist"),
+    path: path.resolve(__dirname, "public", "dist"),
     filename: "[name].js",
     assetModuleFilename: "assets/[name][ext]",
   },
